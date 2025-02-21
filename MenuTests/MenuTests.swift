a233e68f72be808f6ab07d81e0d3c99e3445b258
Abdulkabir <abdulkabir@circo.live>
2025-02-21 11:29:17 +0100

    Initial Commit

---
+++ MenuTests/MenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTests.swift
+//  MenuTests
+//
+//  Created by Circo MAc on 21/02/2025.
+//
+
+import Testing
+@testable import Menu
+
+struct MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
